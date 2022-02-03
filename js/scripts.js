d47af8b5ef53011b2ffb708f079f1cf797bf0541
brianasingh <Briana.singh@snhu.edu>
2022-02-02 21:56:57 -0500

    Created array, listed three pokemon with three attributes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+pokemonList = [{
+    name: "Bulbasaur",
+    height: 7,
+    type: ["Monster", "Grass"]
+  },
+
+  {
+    name: "Jigglypuff",
+    height: 5,
+    type: ["Fairy"]
+  },
+
+  {
+    name: "Squirtle",
+    height: 5,
+    type: ["Monster", "Water"]
+  }
+
+];
