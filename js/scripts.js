f81ff3ee8f4eeb8a070679f80866bb190a697b33
brianasingh <Briana.singh@snhu.edu>
2022-02-03 20:24:03 -0500

    changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,10 @@ pokemonList = [{
   }
 
 ];
+
+for (let i=0; i < pokemonList.length; i++)
+  if(pokemonList[i].height < 7) && pokemonList[i].height > 4 {
+    document.write(pokemonList[i].name + "Is on the smaller side.")
+  } else {
+    document.write(pokemobList[i].name + "Wow, that's big!")
+  };
