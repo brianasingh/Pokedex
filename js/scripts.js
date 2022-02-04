3096693f70e9183be22c859e54d87905b4c5b172
brianasingh <Briana.singh@snhu.edu>
2022-02-04 14:04:38 -0500

    fixed scripts.js file for submission 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,11 @@ pokemonList = [{
 
 ];
 
-for (let i=0; i < pokemonList.length; i++)
-  if(pokemonList[i].height < 7) && pokemonList[i].height > 4 {
+//going to comment out code below for 1.2 task submission
+
+/* for (let i=0; i < pokemonList.length; i++)
+  if(pokemonList[i].height < 7 && pokemonList[i].height > 4) {
     document.write(pokemonList[i].name + "Is on the smaller side.")
   } else {
-    document.write(pokemobList[i].name + "Wow, that's big!")
-  };
+    document.write(pokemonList[i].name + "Wow, that's big!")
+  };*/
