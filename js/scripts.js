cbc3e59d143cf663e6b4d537906e09b71f9692ec
brianasingh <Briana.singh@snhu.edu>
2022-02-05 17:51:49 -0500

    removed excess space

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,10 @@ pokemonList = [{
 
 ];
 
-
 for (let i=0; i < pokemonList.length; i++)
   if(pokemonList[i].height < 7 && pokemonList[i].height > 4) {
     document.write(pokemonList[i].name + ": is on the smaller side.")
   } else {
     document.write(pokemonList[i].name + ": wow, that's big!")
   };
+  
